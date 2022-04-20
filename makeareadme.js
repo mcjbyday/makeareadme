37eaf8e1a101fd43cf800802fe7c58e9a44ea348
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-20 15:52:11 -0500

    removed unnecessary comments

--- makeareadme.js
+++ makeareadme.js
@@ -1,7 +1,7 @@
 // to do store inputs from inquirer into an object
 let fs = require('fs');
 const inquirer = require('inquirer');
-
+// store license options as an array to be referenced by inquirer
 licenses = [
     'Apache License 2.0',
     'GNU General Public License v3.0',
@@ -11,7 +11,7 @@ licenses = [
     'Eclipse Public License 2.0',
     'Mozilla Public License 2.0',
 ];
-
+// inquirer uses promises 
 inquirer
   .prompt([
     {
@@ -64,11 +64,9 @@ inquirer
         console.log(JSON.stringify(answers, null, 2));
 })
 .then((response) => {
-
+    // deconstruct response object in order to display information in readme file to be written
     let {projectNameInput, projectLicense, projectDescription, commandLineInstallInput, instructProjectUsage, instructProjectContributions, commandLineTestInput, usernameInput, emailAddressInput} = response;
   
-
-
     fs.writeFile('README_output.md',
 `<h1 id="project-title">${projectNameInput}</h1>\n 
 <img src="https://img.shields.io/badge/license-${projectLicense}-blue.svg">\n
@@ -106,18 +104,3 @@ inquirer
 <p style='text-align: right;'><a href="#project-title">Back to Top</a></p>`, 
     (err) => err ? console.error(err) : console.log('Check directory for details'))
 });
-
-
- 
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
